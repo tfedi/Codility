d116ef022cacd37b3a11581f4dbeb7fbaaa3d475
Ivan Franco <ifranco@tupaca.com>
2018-12-03 23:33:00 -0300

    MissingInteger exercise (demo test)

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Codility
 ## Codility exercises
 
-1. BinaryGap (100 %)
-2. CyclicRotation (100 %)
-3. OddOccurrencesInArray (100 %)
-4. PermMissingElem (100 %)
-5. FrogJmp (100 %)
-6. TapeEquilibrium (100 %)
-7. PermCheck (100 %)
-8. FrogRiverOne (100 %)
-9. MaxCounter (77 %)
+1.  BinaryGap (100 %)
+2.  CyclicRotation (100 %)
+3.  OddOccurrencesInArray (100 %)
+4.  PermMissingElem (100 %)
+5.  FrogJmp (100 %)
+6.  TapeEquilibrium (100 %)
+7.  PermCheck (100 %)
+8.  FrogRiverOne (100 %)
+9.  MaxCounter (77 %)
+10. MissingInteger (100 %) - Demo Test
---
+++ TestDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            solution.solution(new[] { 1, 3, 6, 4, 1, 2 });
+        }
+
+        /*This is a demo task.
+           
+           Write a function:
+           
+           class Solution { public int solution(int[] A); }
+           
+           that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+           
+           For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+           
+           Given A = [1, 2, 3], the function should return 4.
+           
+           Given A = [−1, −3], the function should return 1.
+           
+           Write an efficient algorithm for the following assumptions:
+           
+           N is an integer within the range [1..100,000];
+           each element of array A is an integer within the range [−1,000,000..1,000,000].
+         */
+
+        class Solution
+        {
+            public int solution(int[] A)
+            {
+                // write your code in C# 6.0 with .NET 4.5 (Mono)
+                var list = A.Where(x => (long)x > 0).OrderBy(x=>x).Distinct().ToList();
+                if (list.Count == 0)
+                    return 1;
+                int i;
+
+                for (i = 0; i < list.Count; i++)
+                {
+                    if (list[i] != i + 1)
+                        return i + 1;
+                }
+
+                return i+1;
+            }
+        }
+    }
+}
